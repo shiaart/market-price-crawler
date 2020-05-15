9d3d2aa251a8a358863db806e0b0b5e243f900ba
shiaart <shiabutdinov@gmail.com>
2020-05-15 13:16:24 +0300

    redis client

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import OpenAPI, { MarketInstrument, MarketInstrumentList } from '@tinkoff/invest-openapi-js-sdk';
 import * as dotenv from "dotenv";
 import fs from 'fs';
+import * as redis from "redis";
 
 dotenv.config();
 const sandboxApiURL = 'https://api-invest.tinkoff.ru/openapi/sandbox/';
@@ -14,12 +15,15 @@ const round = function (value:number, decimals:number) {
     return Number.parseFloat(value.toFixed(decimals));
 }
 
+const red = redis.createClient(6379);
+
 let printPrices = function(figi:string) { 
     api.candle({ figi,  interval: '3min' }, (x) => {
         // console.log('-------------------------');
         // console.log('time: ' + (new Date()).toString());
         // console.log(x.h);
-        
+        console.log(x.figi + '\n');
+        red.set(x.figi, x.h.toString());
         if(round(x.h, 0)/x.h - 1 < 0.05){
             writeStream.write('h:' + x.h + ' l:'+ x.l + '\n');
         }
