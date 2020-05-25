4349acb3f56438e19e26815b25faf91a7b073d49
shiaart <shiabutdinov@gmail.com>
2020-05-25 22:56:46 +0300

    new candle output, debug config

--- index.ts
+++ index.ts
@@ -8,38 +8,45 @@ const sandboxApiURL = 'https://api-invest.tinkoff.ru/openapi/sandbox/';
 const socketURL = 'wss://api-invest.tinkoff.ru/openapi/md/v1/md-openapi/ws';
 const sandboxToken = process.env.SANDBOX_TOKEN; // токен для сандбокса
 const api = new OpenAPI({ apiURL: sandboxApiURL, secretToken: sandboxToken as string, socketURL });
+const usList = ['AAPL', 'TXN', 'NEE', 'UNH', 'LMT', 'ACN', 'HD', 'V', 'MCD', 'ROST'];
+const ruList = ['SBER'];//, 'TCSG', 'TATN', 'TATNP', 'DSKY', 'LKOH', 'MOEX', 'MTSS', 'CHMF', 'PHOR', 'GAZP', 'UPRO', 'FXWO'];
+// let writeStream = fs.createWriteStream('./secret.txt');
 
-let writeStream = fs.createWriteStream('./secret.txt');
+// const round = function (value:number, decimals:number) {
+//     return Number.parseFloat(value.toFixed(decimals));
+// }
 
-const round = function (value:number, decimals:number) {
-    return Number.parseFloat(value.toFixed(decimals));
-}
+//const red = redis.createClient(6379);
 
-const red = redis.createClient(6379);
+let printPrices = function(inst:MarketInstrument) { 
+    console.log('getting candle for:' + inst.ticker);
+    const figi = inst.figi;
+    
+    api.candle({ figi, interval: 'day' }, (x) => {
+        console.log(inst.ticker + ' ' + x.interval + 
+        ' low:'+ x.l + 
+        ' high:'+ x.h + 
+        ' vol:'+ x.v +
+        '\n');
 
-let printPrices = function(figi:string) { 
-    api.candle({ figi,  interval: '3min' }, (x) => {
-        // console.log('-------------------------');
-        // console.log('time: ' + (new Date()).toString());
-        // console.log(x.h);
-        console.log(x.figi + '\n');
-        red.set(x.figi, x.h.toString());
-        if(round(x.h, 0)/x.h - 1 < 0.05){
-            writeStream.write('h:' + x.h + ' l:'+ x.l + '\n');
-        }
 
-       
     });
 };
 !(async function run() {
-    let stockList = await api.stocks() as MarketInstrumentList;
-    stockList.instruments.forEach(inst => writeStream.write(inst.ticker+';'));
-    writeStream.write('\n---------------------------------------------------\n');
-    stockList.instruments.forEach(x => {
-        printPrices(x.figi);
+    let stockList = await Promise.all(ruList.map(async (stock): Promise<MarketInstrument>=> {
+        return await api.searchOne({ ticker: stock }) as MarketInstrument;
+    })); 
+    
+    stockList.forEach(inst => console.log(inst.ticker+';'));
+    
+    // const figi  = stockList[0].figi;
+    // const unsubFromAAPL = api.orderbook({ figi }, (ob) => { console.log(ob.bids) });
+
+    stockList.forEach(x => {
+        printPrices(x);
     });
 })();  
 
-writeStream.on('finish', () => {
-    console.log('wrote all data to file');
-});
+// writeStream.on('finish', () => {
+//     console.log('wrote all data to file');
+// });
